8c389345f9b3d4b284f0f65cb063c5d4b55177f6
nomis2001sk <simon.koefinger@hotmail.com>
2024-10-28 17:53:22 +0100

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="STS1_sensor_libraries",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "smbus2"
