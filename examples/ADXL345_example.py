8de0743eabe3d93ae38794c752e4fc7632c0f04a
Florian Rohrer <semicolonator@gmail.com>
2024-12-10 23:03:29 +0100

    Calibration example added

--- examples/ADXL345_example.py
+++ examples/ADXL345_example.py
@@ -5,7 +5,7 @@ from sts1_sensor_libraries import ADXL345
 log = structlog.get_logger()
 
 accel = ADXL345(address=0x53, range=2, datarate=3200, 
-                x_offset=-0.06, y_offset=0.03, z_offset=0.06)
+                x_offset=-0.04570, y_offset=-0.00697, z_offset=0.04614)
 
 while True:
     x, y, z = accel.get_g()
