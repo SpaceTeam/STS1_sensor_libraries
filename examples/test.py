6bca3771cbd40e1c2020638761ece6c8c2c917d3
Florian Rohrer <semicolonator@gmail.com>
2024-12-10 18:03:03 +0100

    updates

--- examples/test.py
+++ examples/test.py
@@ -1,12 +1,10 @@
 from sts1_sensor_libraries import ADXL345
-from smbus2 import SMBus
 import time
 
-with SMBus(1) as bus:
-    accel = ADXL345(bus, address=0x53, range=2, datarate=3200, 
-                    x_offset=-0.06, y_offset=0.03, z_offset=0.06)
-    
-    while True:
-        x, y, z = accel.get_g()
-        print(f"X: {x:.2f}g, Y: {y:.2f}g, Z: {z:.2f}g")
-        time.sleep(.1)
+accel = ADXL345(address=0x53, range=2, datarate=3200, 
+                x_offset=-0.06, y_offset=0.03, z_offset=0.06)
+
+while True:
+    x, y, z = accel.get_g()
+    print(f"X: {x:.2f}g, Y: {y:.2f}g, Z: {z:.2f}g")
+    time.sleep(.1)
--- src/sts1_sensor_libraries/ADXL345.py
+++ src/sts1_sensor_libraries/ADXL345.py
@@ -1,220 +1,84 @@
-import logging
+from smbus2 import SMBus
 
 class ADXL345:
-    poss_addr = [0x1D, 0x3A, 0x3B, 0x53]
-    poss_datarate = [0.10,0.2,0.39,0.78,1.56,3.13,6.25,12.5,25,50,100,200,400,800,1600,3200]
-    poss_datarate_bin= [0b0000,0b0001,0b0010,0b0011,0b0100,0b0101,0b0110,0b0111,0b1000,0b1001,0b1010,0b1011,0b1100,0b1101,0b1110,0b1111]
-    poss_range = [2,4,8,16]
-    poss_range_bin = [0b00, 0b01, 0b10, 0b11]
-    range_resolution = [0b1111111111,0b1111111111,0b1111111111,0b1111111111]
+    possible_addresses = [0x1D, 0x3A, 0x3B, 0x53]
+    possible_datarates = [0.10, 0.20, 0.39, 0.78, 1.56, 3.13, 6.25, 12.5, 25, 50, 100, 200, 400, 800, 1600, 3200]
+    possible_ranges = [2, 4, 8, 16]
+
+    def __init__(self, bus=None, address=0x53, range=2, datarate=3200, x_offset=0, y_offset=0, z_offset=0):
+        if bus is None:
+            self.bus = SMBus(1)
+            self.manage_bus = True
+        else:
+            self.bus = bus
+            self.manage_bus = False
+            
+        self.address = address
+        self.datarate = datarate
+        self.range = range
+        self.offsets = {"x": x_offset, "y": y_offset, "z": z_offset}
+        self.xyz_addresses = {"x": 0x32, "y": 0x34, "z": 0x36}
+
+        self.bus.write_byte_data(self.address, 0x2C, bin(self.possible_datarates.index(self.datarate)))
+        self.bus.write_byte_data(self.address, 0x2D, 0b1000)
+        self.bus.write_byte_data(self.address, 0x31, 0b1011 & bin(self.possible_ranges.index(self.range)))
+
+    def __del__(self):
+        if self.manage_bus:
+            self.bus.close()
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, address):
+        if address not in self.possible_addresses:
+            s = f"The address {hex(address)} does not exist."
+            s += f" Choose one of {self.possible_addresses}."
+            raise ValueError(s)
+        self._address = address
+
+    @property
+    def datarate(self):
+        return self._datarate
+
+    @datarate.setter
+    def datarate(self, datarate):
+        if datarate not in self.possible_datarates:
+            s = f"The datarate {hex(datarate)} does not exist."
+            s += f" Choose one of {self.possible_datarates}."
+            raise ValueError(s)
+        self._datarate = datarate
+
+    @property
+    def range(self):
+        return self._range
+
+    @range.setter
+    def range(self, range):
+        if range not in self.possible_ranges:
+            s = f"The range {hex(range)} does not exist."
+            s += f" Choose one of {self.possible_ranges}."
+            raise ValueError(s)
+        self._range = range
+
+    def _get_g_raw(self, var):
+        lsb, msb = self.bus.read_i2c_block_data(self.address, self.xyz_addresses[var], 2)
+        k = (msb << 8) | lsb
+        if (k >> 15) == 1:
+            k = (1 << 15) - (k & 0b111111111111111)
+            k = k * (-1)                
+        return k
     
-    config_set = 0
-    addr = 0
-    datarate = 0
-    range = 0
-    resolution = 0
-    check = 0
-    setRange = False
-    setDatarate = False
-    setAddr = False
-    setupD = False
-    Error = False
-    consoleLog = True
+    def get_g_raw(self):
+        return self._get_g_raw("x"), self._get_g_raw("y"), self._get_g_raw("z")
+    
+    def _get_g(self, var):
+        k = self._get_g_raw(var)
+        k = (k / (0b111111111)) * self.range
+        k = k + self.offsets[var]
+        return k
 
-    xoff = 0
-    yoff = 0
-    zoff = 0
-    def __init__(self, bus):
-        self.addr = 0x53
-        self.bus = bus
-    def deact_consoleLog(self):
-        self.consoleLog = False
-    def set_address(self, address):
-        try:
-            self.poss_addr.index(address)
-            self.addr = address
-            self.setAddr = True
-        except ValueError:
-            s = "ADXL345: The address (" + str(hex(address)) + ") you entered for the sensor ADXL345 does not exist!"
-            if self.consoleLog:
-                #print(s)
-                logging.error(s)
-            s = "ADXL345: Try one of the following:"
-            for value in self.poss_addr:
-                s = s + str(hex(value)) + " "
-            if self.consoleLog:
-                #print(s)
-                logging.info(s)
-                print("ADXL345: not initialized!!!")
-                logging.error("ADXL345: not initialized!!!")
-    def set_datarate(self, rate):
-        try:
-            self.poss_datarate.index(rate)
-            self.datarate = rate
-            self.setDatarate = True
-        except ValueError:
-            s = "ADXL345: The datarate (" + str(rate) + ") you entered for the sensor ADXL345 does not exist!"
-            if self.consoleLog:
-                #print(s)
-                logging.error(s)
-            s = "ADXL345: Try one of the following:"
-            for value in self.poss_datarate:
-                s = s + str(value) + " "
-            if self.consoleLog:
-                #print(s)
-                logging.info(s)
-                #print("ADXL345 datarate not set!!!")
-                logging.error("ADXL345 datarate not set!!!")
-    def set_range(self, range):
-        try:
-            self.poss_range.index(range)
-            self.range = range
-            self.resolution = self.range_resolution[self.poss_range.index(range)]
-            self.setRange = True
-        except ValueError:
-            s = "ADXL345: The range (" + str(range) + ") you entered for the sensor ADXL345 does not exist!"
-            if self.consoleLog:
-                #print(s)
-                logging.error(s)
-            s = "ADXL345: Try one of the following:"
-            for value in self.poss_range:
-                s = s + str(value) + " "
-            if self.consoleLog:
-                logging.info(s)
-                logging.error("ADXL345 range not set!!!")
-    def set_offset(self, x, y, z):
-        self.xoff = x
-        self.yoff = y
-        self.zoff = z
-    def setup(self):
-        if self.setAddr and self.setDatarate and self.setRange:
-            #all settings correct#
-            try:
-                self.bus.write_byte_data(self.addr, 0x2C, self.poss_datarate_bin[self.poss_datarate.index(self.datarate)])
-                self.bus.write_byte_data(self.addr, 0x2D, 0b00001000)
-                self.bus.write_byte_data(self.addr, 0x31, 0b00001011 & self.poss_range_bin[self.poss_range.index(self.range)])
-            except OSError as e:
-                self.Error = True
-                if e.errno == 121:
-                    logging.error("ADXL345: Remote I/O Error: The device is not responding on the bus. Therefore it will be ignored")
-                else:
-                    logging.error(f"ADXL345: An error occurred: {e}")
-                return None
-            self.setupD = True
-            if self.consoleLog:
-                logging.info("ADXL345: Setup finished, sensor ready.")
-        else:
-            if self.consoleLog:
-                logging.error("ADXL345: Setup failed! Settings incorrect")
-    def getXRaw(self):
-        if self.setupD:
-            x_LSB = self.bus.read_byte_data(self.addr, 0x32)
-            x_MSB= self.bus.read_byte_data(self.addr, 0x33)
-            x = (x_MSB << 8) | (x_LSB << 0)
-            if (x>>(16-1)) == 0b1:
-                x = (1<<15) - (x & 0b0111111111111111)
-                x = x * (-1)                
-            return x
-        else:
-            if self.consoleLog:
-                if self.Error:
-                    logging.error("ADXL345: not available")
-                else:
-                    logging.error("ADXL345: Setup not finished")
-    def getYRaw(self):
-        if self.setupD:
-            y_LSB = self.bus.read_byte_data(self.addr, 0x34)
-            y_MSB= self.bus.read_byte_data(self.addr, 0x35)
-            y = (y_MSB << 8) | (y_LSB << 0)
-            if (y>>(16-1)) == 0b1:
-                y = (1<<15) - (y & 0b0111111111111111)
-                y = y * (-1)
-            return y
-        else:
-            if self.consoleLog:
-                if self.Error:
-                    logging.error("ADXL345: not available")
-                else:
-                    logging.error("ADXL345: Setup not finished")
-    def getZRaw(self):
-        if self.setupD:
-            z_LSB = self.bus.read_byte_data(self.addr, 0x36)
-            z_MSB= self.bus.read_byte_data(self.addr, 0x37)
-            z = (z_MSB << 8) | (z_LSB << 0)
-            if (z>>(16-1)) == 0b1:
-                z = (1<<15) - (z & 0b0111111111111111)
-                z = z * (-1)
-            return z            
-        else:
-            if self.consoleLog:
-                if self.Error:
-                    logging.error("ADXL345: not available")
-                else:
-                    logging.error("ADXL345: Setup not finished")
-    def getXGs(self):
-        if self.setupD:
-            xnew = self.bus.read_i2c_block_data(self.addr, 0x32, 2)
-            x_MSB = xnew[1]
-            x_LSB = xnew[0]
-            #x_LSB = self.bus.read_byte_data(self.addr, 0x32)
-            #x_MSB= self.bus.read_byte_data(self.addr, 0x33)
-            x = (x_MSB << 8) | (x_LSB << 0)
-            if (x>>(16-1)) == 0b1:
-                x = (1<<15) - (x & 0b0111111111111111)
-                x = x * (-1)
-            #if (x & (1<<(16-1))):
-                #x = x - (1<<16)
-            x = (x / (self.resolution>>1)) * self.range
-            x = x + self.xoff
-            return x
-        else:
-            if self.consoleLog:
-                if self.Error:
-                    logging.error("ADXL345: not available")
-                else:
-                    logging.error("ADXL345: Setup not finished")
-    def getYGs(self):
-        if self.setupD:
-            ynew = self.bus.read_i2c_block_data(self.addr, 0x34, 2)
-            y_MSB = ynew[1]
-            y_LSB = ynew[0]
-            #y_LSB = self.bus.read_byte_data(self.addr, 0x34)
-            #y_MSB= self.bus.read_byte_data(self.addr, 0x35)
-            y = (y_MSB << 8) | (y_LSB << 0)
-            if (y>>(16-1)) == 0b1:
-                y = (1<<15) - (y & 0b0111111111111111)
-                y = y * (-1)
-            #if (y & (1<<(16-1))):
-                #y = y - (1<<16)
-            y = (y / (self.resolution>>1)) * self.range
-            y = y + self.yoff
-            return y
-        else:
-            if self.consoleLog:
-                if self.Error:
-                    logging.error("ADXL345: not available")
-                else:
-                    logging.error("ADXL345: Setup not finished")
-    def getZGs(self):
-        if self.setupD:
-            znew = self.bus.read_i2c_block_data(self.addr, 0x36, 2)
-            z_MSB = znew[1]
-            z_LSB = znew[0]
-            
-            #z_LSB = self.bus.read_byte_data(self.addr, 0x36)
-            #z_MSB= self.bus.read_byte_data(self.addr, 0x37)
-            z = (z_MSB << 8) | (z_LSB << 0)
-            
-            if (z>>(16-1)) == 0b1:
-                z = (1<<15) - (z & 0b0111111111111111)
-                z = z * (-1)
-
-            z = (z / (self.resolution>>1)) * self.range
-            z = z + self.zoff
-            return z
-        else:
-            if self.consoleLog:
-                if self.Error:
-                    logging.error("ADXL345: not available")
-                else:
-                    logging.error("ADXL345: Setup not finished")
+    def get_g(self):
+        return self._get_g("x"), self._get_g("y"), self._get_g("z")
