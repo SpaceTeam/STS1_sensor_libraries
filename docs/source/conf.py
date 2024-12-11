31bba278139ce09c79914f464253c291729f0b5e
Florian Rohrer <semicolonator@gmail.com>
2024-12-12 00:12:51 +0100

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
 project = 'sts1-sensor-libraries'
 copyright = '2024, Simon Köfinger, Florian Rohrer'
 author = 'Simon Köfinger, Florian Rohrer'
