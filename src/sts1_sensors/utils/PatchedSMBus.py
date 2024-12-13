e58342a308a93acd9f5ae3f3d1bba4291a60ab3f
Florian Rohrer <semicolonator@gmail.com>
2024-12-13 21:43:25 +0100

    BMM150 rewrite

---
+++ src/sts1_sensors/utils/PatchedSMBus.py
@@ -0,0 +1,15 @@
+from smbus2 import SMBus
+
+class PatchedSMBus:
+    def __init__(self, bus_number, address):
+        self.bus = SMBus(bus_number)
+        self.address = address
+
+    def read_byte_data(self, ignored, *args, **kwargs):
+        return self.bus.read_byte_data(self.address, *args, **kwargs)
+
+    def write_byte_data(self, ignored, *args, **kwargs):
+        return self.bus.write_byte_data(self.address, *args, **kwargs)
+
+    def read_i2c_block_data(self, ignored, *args, **kwargs):
+        return self.bus.read_i2c_block_data(self.address, *args, **kwargs)
