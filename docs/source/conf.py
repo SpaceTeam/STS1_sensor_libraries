70f711b3d63ae7ed323cae9a35716048c62efe73
Florian Rohrer <semicolonator@gmail.com>
2024-12-13 10:55:06 +0100

    verion bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'sts1-sensors'
 copyright = '2024, Simon Köfinger, Florian Rohrer'
 author = 'Simon Köfinger, Florian Rohrer'
-release = 'v0.3.4'
+release = 'v0.3.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
