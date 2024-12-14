fcbb7121bc1394ed314e60e96fa952485aa3deff
Florian Rohrer <semicolonator@gmail.com>
2024-12-14 17:46:24 +0100

    doc update

--- src/sts1_sensors/edu/ADXL345.py
+++ src/sts1_sensors/edu/ADXL345.py
@@ -22,10 +22,10 @@ class ADXL345(AbstractSensor):
         Example:
 
         .. code-block:: python
-           
-        accel = ADXL345(range=2, datarate=50)
-        x, y, z = accel.get_g()
-        print(f"{x=:.2f} g, {y=:.2f} g, {z=:.2f} g")
+
+           accel = ADXL345(range=2, datarate=50)
+           x, y, z = accel.get_g()
+           print(f"{x=:.2f} g, {y=:.2f} g, {z=:.2f} g")
         """
         super().__init__(possible_addresses=[0x1D, 0x3A, 0x3B, 0x53], bus=bus)
             
